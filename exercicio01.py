ed4a3a170bca3d0e5a36db42771987656abcc740
Jessica Loris <dearjess535@gmail.com>
2025-04-01 09:19:15 -0300

    add calculo do percentual

--- exercicio01.py
+++ exercicio01.py
@@ -1,6 +1,9 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
-salario = float(input("Digite seu salario: "))
+salario = float(input("Digite seu salario atual: "))
+aumento = float(input("Digite a porcentagem de aumento do salário: "))
+valor_real = salario*aumento/100
+novo_salario = salario+valor_real
 
-print(f"Olá{nome}, voce tem {idade} anos e seu salario é {salario}")
+print(f"Olá {nome}, voce tem {idade} anos e seu salario é R${salario:.2f} e voce recebeu R${novo_salario:.2f} de aumento.")
 
