59d45d758457c6584723bcf2bcfc4d5a162616cd
Jessica Loris <dearjess535@gmail.com>
2025-04-08 11:21:30 -0300

    usando for

--- exercicio09.py
+++ exercicio09.py
@@ -1,29 +1,31 @@
 num_mes = int(input("Digite o número do mês de 1 a 12: "))
 
-if num_mes == 1:
-    print("Janeiro")
-elif num_mes == 2:
-    print("Fevereiro")
-elif num_mes == 3:
-    print("Março")
-elif num_mes == 4:
-    print("Abril")
-elif num_mes == 5:
-    print("Maio")
-elif num_mes == 6:
-    print("Junho")
-elif num_mes == 7:
-    print("Julho")
-elif num_mes == 8:
-    print("Agosto")
-elif num_mes == 9:
-    print("Setembro")
-elif num_mes == 10:
-    print("Outubro")
-elif num_mes == 11:
-    print("Novembro")
-elif num_mes == 12:
-    print("Dezembro")
-
+if num_mes < 1 or num_mes > 12:
+    print("Que parte você não entendeu?")
 else:
-    print("Valor inválido")
+    if num_mes == 1:
+         print("Janeiro")
+    elif num_mes == 2:
+        print("Fevereiro")
+    elif num_mes == 3:
+        print("Março")
+    elif num_mes == 4:
+        print("Abril")
+    elif num_mes == 5:
+        print("Maio")
+    elif num_mes == 6:
+        print("Junho")
+    elif num_mes == 7:
+        print("Julho")
+    elif num_mes == 8:
+        print("Agosto")
+    elif num_mes == 9:
+        print("Setembro")
+    elif num_mes == 10:
+        print("Outubro")
+    elif num_mes == 11:
+        print("Novembro")
+    elif num_mes == 12:
+        print("Dezembro")
+    else:
+        print("Valor inválido")
